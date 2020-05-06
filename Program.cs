4ca26e070f90f2ba1ecf250b503ed86aa017b7d6
Randy Chan <randychan997@gmail.com>
2020-05-06 00:20:31 -0500

    Finished program.

--- Program.cs
+++ Program.cs
@@ -6,7 +6,65 @@ namespace assignment2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Global Declarations
+            int daysToStay;
+            string addonCode;
+            //Main line
+            welcomeMod();
+            Console.WriteLine("How many days will your dog be staying with us?");
+            daysToStay = Convert.ToInt32(userInputReq());
+            Console.WriteLine("Would you like a service package addon with that too?");
+            Console.WriteLine("If not, please leave it blank and hit enter.");
+            Console.WriteLine("Here are the service codes...");
+            Console.WriteLine("N or blank = No Service addon, + $0.00.");
+            Console.WriteLine("A = Bathing and Grooming, + $169.00 per day.");
+            Console.WriteLine("C = Bathing, + $112.00 per day.");
+            addonCode = userInputReq();
+            if(string.IsNullOrEmpty(addonCode) || addonCode == "N" || addonCode == "n"){
+                computeRate(daysToStay);
+            }
+            else{
+                computeRate(daysToStay, addonCode);
+            }
+        }
+        // Welcome Module
+        public static void welcomeMod(){
+            Console.WriteLine("MPLS Dog Boarding");
+            Console.WriteLine("-----------------");
+            Console.WriteLine("Hello Customer!");
+            Console.WriteLine("Thank you for using our rate computation program!");
+        }
+        // User input
+        public static string userInputReq(){
+            string input = Console.ReadLine();
+            return input;
+        }
+        // Deals with rate computation without addon codes.
+        public static void computeRate(int days){
+            int price;
+            price = days * 79;
+            Console.WriteLine("Here is your final Bill!");
+            Console.WriteLine($"Your dog will be staying with us for {days} days.");
+            Console.WriteLine("Your Addon Service code is N, which means no addon package was selected.");
+            Console.WriteLine($"Your total bill is ${price}");
+        }
+        // Deals with rate computation with addon codes.
+        public static void computeRate(int days, string code){
+            int price;
+            if(code == "A" || code == "a"){
+                price = (169 + 79) * days;
+                Console.WriteLine("Here is your final Bill!");
+                Console.WriteLine($"Your dog will be staying with us for {days} days.");
+                Console.WriteLine("Your Addon Service code is A, bathing and grooming is included");
+                Console.WriteLine($"Your total bill is ${price}");
+            }
+            else if(code == "C" || code == "c"){
+                price = (112 + 79) * days;
+                Console.WriteLine("Here is your final Bill!");
+                Console.WriteLine($"Your dog will be staying with us for {days} days.");
+                Console.WriteLine("Your Addon Service code is A, bathing and grooming is included");
+                Console.WriteLine($"Your total bill is ${price}");
+            }
         }
     }
 }
---
+++ README.md
@@ -0,0 +1,24 @@
+MPLS Dog Boarding Compute Rate Program
+
+The MPLS Dogboarding academy wants to incorporate functionality that allows the end-user to select add-on services such as grooming and bathing to be included within the total cost of the dog's stay. At the conclusion of the program, the program will output the total number of days the dog will stay, the amenities selected and the total cost.
+
+ (Links to an external site.)Requirements
+Construct a program that allows the end-user to input the number of days the dog will stay and an add-on code. Include two overloaded methods named computeRate(). One version accepts an argument that includes the number of days and calculates the rate at $75.00 per day. The other accepts two arguments - the number of days and a code for add-on services. If the code is A, bathing and grooming will be included, and the price is $169.00 per day. If the code is C, only bathing is included, and the price is $112.00 per day. All other codes are invalid. Each method returns the rate to the calling program where it is displayed. The main program asks the user for the number of days in a stay and whether add-on services should be included; then, based on the user’s response, the program either calls the first method or prompts for a add-on services code and calls the second method. At the conclusion of the program, the program outputs the number of days, the add-on services code and the total cost of stay.
+
+ (Links to an external site.)Submission
+Please submit a zipped folder containing your c# console application and the URL to your Github repository.
+
+ (Links to an external site.)A few considerations:
+Only publish working code to your master/prod branch. Make sure to branch when experimenting with new functionality.
+Submit a zip file to Canvas and submit the URL to your GitHub repository
+ (Links to an external site.)Rubric Criteria
+Program successfully compiles and runs
+Program properly declares variables including applicable constant variables (local and global variables declared, input/output).
+Properly defined overloaded methods, methods have correct arguments/declared parameters; method/module header and return statement is correct;
+Logic of main method is correctly coded with consideration of which method needs to be invoked
+Logic for overloaded method/module is correctly coded with consideration of proper calculation
+Welcome module and end of program module included in program; end of program method module outputs data along with total cost of stay.
+
+Student Notes
+
+Created new console with 3.1.3 standard, couldn't download 1.1.2.
